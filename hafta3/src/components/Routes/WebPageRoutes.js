21a9d776c590a007f1c4fb7f5951e47a28c5081e
murderuo <ugurokur365@hotmail.com>
2022-08-18 00:20:54 +0300

    Routes js de değişiklikler ve notlar eklendi

--- hafta3/src/components/Routes/WebPageRoutes.js
+++ hafta3/src/components/Routes/WebPageRoutes.js
@@ -13,7 +13,9 @@ function AllPageRoutes() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />}></Route>
-          <Route path="/contact/:contactid" element={<Details />} />
+          {/* >>>>>>>>>>>>< */}
+          <Route path="/contact/:contactid" element={<Details />} />{" "}
+          {/* Burda nested route yapmadık ayrıca route ekledik */}
           <Route
             path="*"
             element={
